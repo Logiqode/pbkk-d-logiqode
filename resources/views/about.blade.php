9faf6c23c7a11b271336d539888e7cf9d9414ae4
Logiqode <jeremyjamestprawira@gmail.com>
2024-09-10 22:07:05 +0700

    Tugas 1 PBKK D -rev

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,5 +2,5 @@
     <x-slot:title>{{ $title }}</x-slot:title>
     <h1>Halaman About</h1>
     <h3>Owner: {{ $nama }} </h3>
-    <img src="img/JLPT_Score.png" width="200">
+    <img src="img/JLPT_Score.png" width="600">
 </x-layout>
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,10 +4,10 @@
 
     <ul>
         <li>
-            Instagram: <a href="https://instagram.com/jeremyjtp"></a>
+            Instagram: <a href="https://instagram.com/jeremy_jtp" target="blank">https://instagram.com/jeremy_jtp</a>
         </li>
         <li>
-            LinkedIn: <a href="https://www.linkedin.com/in/jeremy-james-tp/"></a>
+            LinkedIn: <a href="https://www.linkedin.com/in/jeremy-james-tp/" target="blank">https://www.linkedin.com/in/jeremy-james-tp/</a>
         </li>
     </ul>
 </x-layout>
