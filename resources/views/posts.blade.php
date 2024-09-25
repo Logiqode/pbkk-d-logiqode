e95545548e4b89a5e6a353375a4a691aeba1adb4
Logiqode <jeremyjamestprawira@gmail.com>
2024-09-25 18:47:50 +0700

    Database, Migration, Eloquent ORM, Post Model

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -7,9 +7,9 @@
                     <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
                 </a>
                 <div class="text-base text-gray-500">
-                    <a href="#">{{ $post['author'] }}</a> | ２０２４年１月一日
+                    <a href="#">{{ $post['author'] }}</a> | {{ $post->created_at->diffForHumans() }}
                 </div>
-                <p class="my-4 font-light">{{ Str::limit($post['body'], 300)}}
+                <p class="my-4 font-light">{{ Str::limit($post['body'], 150)}}
                 </p>
                 <a href="/posts/{{ $post['slug'] }}" class="font-medium text-blue-500 hover:underline">続きを読む &raquo;</a>
             </article>  
